c9d93b840e9d7061844427e43ca5f371604e1efe
zhangss <zssqqzss@vip.qq.com>
2017-05-14 19:18:07 +0800

    crate project

--- index.js
+++ index.js
@@ -1,64 +1,73 @@
-const https = require("https");
-const fs = require("fs");
-const path = require("path");
-const request = require("request");
-const io = require("cheerio");
+var http = require("http");
+var https = require("https");
+var fs = require("fs");
+var io = require("cheerio");
+var express = require("express");
+var superagent = require("superagent");
+var jQuery = require("jquery");
 
-const login = require("./lib/login.js");
-const daily_rank = require("./lib/daily_rank.js");
+var target_url = "https://www.pixiv.net/ranking.php?mode=daily&content=illust";
 
-const imgHead = "https://i.pximg.net/img-original";
-const pixiv_url = "https://www.pixiv.net/";
+var fileNum = Math.floor(Math.random() * 100);
 
-var cookie;
+https.get(target_url, function(res) {
+    var date = new Date();
+    var day = date.getDate();
+    var mouth = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var html = "";
 
-var _date = process.argv.slice(2)[0]; //自定义时间 20170101,长度为8
-var today = new Date();
-today.setTime(today - 24 * 60 * 60 * 1000);
-var day = JSON.stringify(today.getDate()).length < 2 ? "0" + today.getDate() : today.getDate();
-var month = JSON.stringify(today.getMonth() + 1).length < 2 ? "0" + (today.getMonth() + 1) : today.getMonth() + 1;
-var year = today.getFullYear();
-var date = _date || year + month + day;
+    res.on("data", function(chunk) {
+        html += chunk;
+    });
 
-//set cookie and save it.when some page need cookie,use it.
-fs.exists("cookie/pixiv.txt", function(exists) {
-    if (exists) {
-        //var cookie = fs.readFileSync("cookie/pixiv.txt", "utf-8");
-        fs.readFile("cookie/pixiv.txt", "utf-8", function(err, res) {
+    res.on("end", function() {
+        var $ = io.load(html);
+        var content = $("section h2 a");
+        var picture_href = pictureHref($, content);
 
-            cookie = res;
-            console.log(cookie + "\n");
-
-            var picture_path = fs.existsSync("./picture");
-            if (!picture_path) {
-                fs.mkdirSync("./picture");
-            }
-
-            if (date.length == 8) {
-                daily_rank(cookie, date);
-            } else {
-                console.log("输入的日期格式不正确");
-            }
-
-
-            //test cookie 
-            var jar = request.jar();
-            jar.setCookie(cookie, pixiv_url);
-            request({
-                url: pixiv_url,
-                headers: {
-                    'Referer': pixiv_url,
-                    'User-Agent': "Mozilla/5.0 (Windows NT 6.3; rv:27.0) Gecko/20100101 Firefox/27.0",
-                },
-                jar: jar
-            }, function(err, res, body) {
-                fs.writeFile("./log/pixiv.html", body, "utf-8", function() {});
-            });
-
+        fs.writeFile("log/pixiv_rank" + year + mouth + day + "_" + fileNum + ".txt", JSON.stringify(picture_href), "utf-8", function(err) {
+            if (err) throw err;
         });
-    } else {
-        cookie = login();
-        console.log(cookie);
-        console.log("cookie已建立，再次运行开始正常操作XD");
-    }
+    });
 })
+
+function pictureHref($, content) {
+    var pics = {};
+    content.each(function(index, tag) {
+        pics[index] = {
+            "title": tag.children[0].data,
+            "href": "https://www.pixiv.net/" + tag.attribs.href
+        };
+    });
+    return pics;
+}
+
+function savePicture($, src) {
+    https.get(src, function(res) {
+        var data = "";
+
+        res.setEncoding("binary");
+        res.on("data", function(chunk) {
+            data += chunk;
+        }).on("end", function() {
+            fs.writeFile("picture/test.jpg", data, "binary", function(err) {
+                if (err) throw err;
+            });
+        })
+    })
+}
+
+function saveText($, src) {
+    https.get(src, function(res) {
+        var data = "";
+
+        res.on("data", function(chunk) {
+            data += chunk;
+        }).on("end", function() {
+            fs.writeFile("data/text.txt", data, "utf-8", function(err) {
+                if (err) throw err;
+            });
+        });
+    })
+}
